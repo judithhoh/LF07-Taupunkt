3bb2681126f47fe9094b24b7bfcb701709be02e5
Judith <judith.hoellwarth@gmx.de>
2026-01-20 09:01:25 +0100

    DHT11 Text Code ohne abhängigkeiten TaupunktMain als einstiegpunkt für das gesamt Programm

--- TaupunktMain.py
+++ TaupunktMain.py
@@ -1,5 +1,19 @@
+import time
+import DHT11
+import LcdDisplay
 
+def main():
+    sensor = DHT11.dht11Sensor()
+    display = LcdDisplay.LcdDisplay()
+    try:
+        while True:
+            display.nachricht(sensor.lesen())
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        # LCD ausschalten.
+        display.lcd.clear()
+        display.hintergund_aus()
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    main()
