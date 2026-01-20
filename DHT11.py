baf3b160f365307b67030b0484b4057aec06b5f6
Judith <judith.hoellwarth@gmx.de>
2026-01-20 08:38:04 +0100

    Ausgabe in schleife => display war kein Objekt sondern die Klasse

--- DHT11.py
+++ DHT11.py
@@ -9,13 +9,16 @@ GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 # lesen der Daten ueber Pin 4
 instance = dht11.DHT11(pin = 4)
-result = instance.read()
-while not result.is_valid(): # lesen bis Werte ok sind
-    result = instance.read()
+display = LcdDisplay.LcdDisplay()
+try:
+    while True:
+        result = instance.read()
+        while not result.is_valid(): # lesen bis Werte ok sind
+            result = instance.read()
+        nachricht = "Temperature: %-3.1f C" % result.temperature +"\nHumidity: %-3.1f %%" % result.humidity
+        display.nachricht(nachricht)
+        time.sleep(1)
 
-display = LcdDisplay.LcdDisplay
-nachricht = "Temperature: %-3.1f C" % result.temperature
-display.nachricht(nachricht)
-time.sleep(1)
-nachricht = "Humidity: %-3.1f %%" % result.humidity
-display.nachricht(nachricht)
+except KeyboardInterrupt:
+    # LCD ausschalten.
+    display.hintergund_aus()
