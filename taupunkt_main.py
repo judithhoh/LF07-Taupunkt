12e72144784e4fff8c923a413667017e51b59ab3
Judith <judith.hoellwarth@gmx.de>
2026-05-12 08:03:14 +0200

    warten bis rely fertig kofiguriert?

--- taupunkt_main.py
+++ taupunkt_main.py
@@ -1,4 +1,6 @@
 import threading
+import time
+
 from app import app
 from taupunkt_scanner import main
 import RPi.GPIO as GPIO
@@ -9,4 +11,5 @@ if __name__ == '__main__':
     t = threading.Thread(target=main)
     t.daemon = True
     t.start()
+    time.sleep(1)
     app.run(host="0.0.0.0", port=5000, debug=True) # vorord nochmal testen
