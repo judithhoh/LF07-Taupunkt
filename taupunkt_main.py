48866c1a3f9c5c6dc0ce831716d817e3ea0b5706
Judith <judith.hoellwarth@gmx.de>
2026-05-12 07:57:14 +0200

    GPIO.Setmode(GPIO.BCM) ausfehren rausgenomne => wieder drinn

--- taupunkt_main.py
+++ taupunkt_main.py
@@ -1,6 +1,9 @@
 import threading
 from app import app
 from taupunkt_scanner import main
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
 
 if __name__ == '__main__':
     t = threading.Thread(target=main)
